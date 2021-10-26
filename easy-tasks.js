9f109fc442ba9c64ae1304ded5c60e9868d152b9
dtr200 <negevski@yandex.ru>
2021-10-26 23:26:30 +0300

    Easy-tasks: Valid parentheses task has been completed

--- easy-tasks.js
+++ easy-tasks.js
@@ -173,3 +173,34 @@ Time complexity - O(N * L), L - shortest string length, N - numbers of strings
     }
     return result;
 };
+
+/*
+20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+Time complexity - O(n), n - string length
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    const stack = [];
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === '(' || s[i] === '[' || s[i] === '{')
+            stack.push(s[i]);
+        else if((s[i] === ')' && stack.slice(-1)[0] === '(') ||
+                (s[i] === ']' && stack.slice(-1)[0] === '[') ||
+                (s[i] === '}' && stack.slice(-1)[0] === '{'))
+            stack.pop();
+        else
+            return false;
+    }
+    return stack.length === 0; 
+};
