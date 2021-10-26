2f8249ee6b42c732198cd700982901d0b278a32c
dtr200 <negevski@yandex.ru>
2021-10-27 01:36:43 +0300

    Easy tasks: task 26 has been completed

--- easy-tasks.js
+++ easy-tasks.js
@@ -204,3 +204,29 @@ Time complexity - O(n), n - string length
     }
     return stack.length === 0; 
 };
+
+/*
+26. Remove Duplicates from Sorted Array
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Time complexity - O(n ** 2), memory complexity - O(1)
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        const last = nums.lastIndexOf(nums[i]);
+        if(last > i)
+            nums.splice(i + 1, last + 1, ...nums.splice(last + 1));
+    }
+    return nums.length;
+};
