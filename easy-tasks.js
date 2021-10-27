73e3af51660390d09fb81a4055a9d65c80901050
dtr200 <negevski@yandex.ru>
2021-10-27 15:28:59 +0300

    Easy tasks: task 27 has been added

--- easy-tasks.js
+++ easy-tasks.js
@@ -209,13 +209,12 @@ Time complexity - O(n), n - string length
 26. Remove Duplicates from Sorted Array
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
-Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
-
 Return k after placing the final result in the first k slots of nums.
 
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
-Time complexity - O(n ** 2), memory complexity - O(1)
+Time complexity - O(n ** 2).
+Memory complexity - O(1)
 */
 
 /**
@@ -229,4 +228,50 @@ Time complexity - O(n ** 2), memory complexity - O(1)
             nums.splice(i + 1, last + 1, ...nums.splice(last + 1));
     }
     return nums.length;
+};
+
+/*
+27. Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Time complexity - O(n/2).
+Memory complexity - O(1).
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    let l = 0,
+        r = nums.length - 1;
+    
+    if(nums.length === 1 && nums[0] !== val)
+        return 1;
+    
+    while(l < r){
+        if(nums[l] === val && nums[r] !== val){
+            [ nums[l], nums[r] ] = [ nums[r], nums[l] ];
+            l++;
+            r--;
+        }
+        else if(nums[l] === val && nums[r] === val){
+            while(nums[r] === val && l < r)
+                r--;
+            if(nums[r] === val) break;
+            [ nums[l], nums[r] ] = [ nums[r], nums[l] ];
+            l++;
+            r--;
+        }
+        else if(nums[l] !== val){
+            l++;
+        }
+    }
+    
+    return nums[l] === val ? l : l + 1;
 };
