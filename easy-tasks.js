ca4c0e01ed8aac130acb0d85a582067821ee9476
dtr200 <negevski@yandex.ru>
2021-10-29 00:27:54 +0300

    Easy tasks: task 69 has been completed

--- easy-tasks.js
+++ easy-tasks.js
@@ -457,3 +457,20 @@ Memory complexity - O(1).
 var addBinary = function(a, b) {
     return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2)
 };
+
+/*
+69. Sqrt(x)
+Given a non-negative integer x, compute and return the square root of x.
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+Time complexity - O(1).
+Memory complexity - O(1).
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x));
+};
