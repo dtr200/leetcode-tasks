bfee831355bb6197199a7d8723b198ad088fab3a
dtr200 <negevski@yandex.ru>
2021-10-28 23:54:12 +0300

    Easy tasks: task 66 has been completed

--- easy-tasks.js
+++ easy-tasks.js
@@ -431,3 +431,17 @@ Memory complexity - O(1).
     }
     return length;
 };
+
+/*
+66. Plus One
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+Time complexity - O(n).
+Memory complexity - O(1).
+*/
+
+var plusOne = function(digits) {
+    return String(BigInt(digits.join('')) + BigInt(1)).split('');
+};
