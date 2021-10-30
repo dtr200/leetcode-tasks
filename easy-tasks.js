4569a5171393465f3748ddc2e6811c94b15b37f3
dtr200 <negevski@yandex.ru>
2021-10-31 00:22:20 +0300

    Easy tasks: task 88 has been added

--- easy-tasks.js
+++ easy-tasks.js
@@ -535,6 +535,33 @@ Memory complexity - O(1).
 };
 
 /*
+88. Merge Sorted Array
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Time complexity - O(K log K + N), K - nums1 length, N - nums2 length.
+Memory complexity - O(1).
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    for(let i = m; i < nums1.length; i++)
+        nums1[i] = nums2[i - m];
+    
+    nums1.sort((a, b) => a - b);
+};
+
+
+/*
 168. Excel Sheet Column Title
 Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
