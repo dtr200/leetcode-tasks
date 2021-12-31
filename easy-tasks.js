2b48a3d17fe0e2b337f154efd40d6bf070414a73
dtr200 <negevski@yandex.ru>
2021-12-31 13:52:56 +0300

    Easy-tasks: task 326 added

--- easy-tasks.js
+++ easy-tasks.js
@@ -1453,6 +1453,24 @@ Space complexity - O(n).
 };
 
 /*
+326. Power of Three
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+Time complexity - O(1).
+Memory complexity - O(1).
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfThree = function(n) {
+    if(n === 0) return false;
+    
+    return Number.isInteger(Math.log10(n)/Math.log10(3)); 
+};
+
+/*
 448. Find All Numbers Disappeared in an Array
 Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
